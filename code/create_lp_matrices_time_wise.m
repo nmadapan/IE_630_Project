8946e7a5cad8ae2a3c29c15df5036fac57351352
Naveen Madapana <naveen.madapana@gmail.com>
2020-04-30 01:48:42 -0400

    re-arranged code

--- code/create_lp_matrices_time_wise.m
+++ code/create_lp_matrices_time_wise.m
@@ -20,7 +20,7 @@ colorbar;
 % D matrix for normal tissue
 K = 5;
 D_N1 = return_dmat(in_normal_flags, in_col_flags, K);
-D_N1 = [D_N1, zeros(size(D_N1, 1), 2 * K)];
+D_N1 = [D_N1, zeros(size(D_N1, 1), 3 * K)];
 Dt_N1 = sum(D_N1, 1);
 Vt_N1 = sum(D_N1~=0) * dV;
 % D matrix for tumor
@@ -28,7 +28,7 @@ D_T1 = return_dmat(in_tumor_flags, in_col_flags, K);
 V_T1 = sum(D_T1~=0) * dV;
 % D matrix for organ at risk
 D_O1 = return_dmat(in_oar_flags, in_col_flags, K);
-D_O1 = [D_O1, zeros(size(D_O1, 1), 2 * K)];
+D_O1 = [D_O1, zeros(size(D_O1, 1), 3 * K)];
 Dt_O1 = sum(D_O1, 1);
 Vt_O1 = sum(D_O1~=0) * dV;
 
@@ -46,7 +46,7 @@ colorbar;
 
 % D matrix for normal tissue
 D_N2 = return_dmat(in_normal_flags, in_col_flags, K);
-D_N2 = [zeros(size(D_N2, 1), K), D_N2, zeros(size(D_N2, 1), K)];
+D_N2 = [zeros(size(D_N2, 1), K), D_N2, zeros(size(D_N2, 1), 2*K)];
 Dt_N2 = sum(D_N2, 1);
 Vt_N2 = sum(D_N2~=0) * dV;
 % D matrix for tumor
@@ -54,7 +54,7 @@ D_T2 = return_dmat(in_tumor_flags, in_col_flags, K);
 V_T2 = sum(D_T2~=0) * dV;
 % D matrix for organ at risk
 D_O2 = return_dmat(in_oar_flags, in_col_flags, K);
-D_O2 = [zeros(size(D_O2, 1), K), D_O2, zeros(size(D_O2, 1), K)];
+D_O2 = [zeros(size(D_O2, 1), K), D_O2, zeros(size(D_O2, 1), 2*K)];
 Dt_O2 = sum(D_O2, 1);
 Vt_O2 = sum(D_O2~=0) * dV;
 
@@ -73,7 +73,7 @@ colorbar;
 
 % D matrix for normal tissue
 D_N3 = return_dmat(in_normal_flags, in_col_flags, K);
-D_N3 = [zeros(size(D_N3, 1), 2 * K), D_N3];
+D_N3 = [zeros(size(D_N3, 1), 2 * K), D_N3, zeros(size(D_N3, 1), K)];
 Dt_N3 = sum(D_N3, 1);
 Vt_N3 = sum(D_N3~=0) * dV;
 % D matrix for tumor
@@ -81,34 +81,58 @@ D_T3 = return_dmat(in_tumor_flags, in_col_flags, K);
 V_T3 = sum(D_T3~=0) * dV;
 % D matrix for organ at risk
 D_O3 = return_dmat(in_oar_flags, in_col_flags, K);
-D_O3 = [zeros(size(D_O3, 1), 2 * K), D_O3];
+D_O3 = [zeros(size(D_O3, 1), 2 * K), D_O3, zeros(size(D_O3, 1), K)];
 Dt_O3 = sum(D_O3, 1);
 Vt_O3 = sum(D_O3~=0) * dV;
 
-D_T = [D_T1, zeros(size(D_T1, 1), 2 * K); ...
-       zeros(size(D_T2, 1), K), D_T2, zeros(size(D_T2, 1), K); ...
-       zeros(size(D_T3, 1), 2*K), D_T3];
-V_T = [V_T1, zeros(size(V_T1, 1), 2 * K); ...
-       zeros(size(V_T2, 1), K), V_T2, zeros(size(V_T2, 1), K); ...
-       zeros(size(V_T3, 1), 2*K), V_T3];
+%% DATA MXY (Minus X Plus Y)
+
+load('data_mxy.mat')
+N_coll = numel(yc_pts) * numel(zc_pts);
+
+% Plot the heatmap
+z = P(:, 3);
+flags = z == 0;
+X = reshape(intensity_list(flags), numel(xe_pts), numel(ye_pts));
+figure;
+imagesc(X)
+colorbar;
+
+% D matrix for normal tissue
+D_N4 = return_dmat(in_normal_flags, in_col_flags, K);
+D_N4 = [zeros(size(D_N4, 1), 3 * K), D_N4];
+Dt_N4 = sum(D_N4, 1);
+Vt_N4 = sum(D_N4~=0) * dV;
+% D matrix for tumor
+D_T4 = return_dmat(in_tumor_flags, in_col_flags, K);
+V_T4 = sum(D_T4~=0) * dV;
+% D matrix for organ at risk
+D_O4 = return_dmat(in_oar_flags, in_col_flags, K);
+D_O4 = [zeros(size(D_O4, 1), 3 * K), D_O4];
+Dt_O4 = sum(D_O4, 1);
+Vt_O4 = sum(D_O4~=0) * dV;
+
+%%
+D_T = [D_T1, D_T2, D_T3, D_T4];
+V_T = [V_T1, V_T2, V_T3, V_T4];
 Dt_T = sum(D_T, 1);
 Vt_T = sum(V_T, 1);
 
-C_N = Dt_N1 + Dt_N2 + Dt_N3;
-C_O = Dt_O1 + Dt_O2 + Dt_O3;
+C_N = Dt_N1 + Dt_N2 + Dt_N3 + Dt_N4;
+C_O = Dt_O1 + Dt_O2 + Dt_O3 + Dt_O4;
 C_T = Dt_T;
 
-V_N = Vt_N1 + Vt_N2 + Vt_N3;
-V_O = Vt_O1 + Vt_O2 + Vt_O3;
+V_N = Vt_N1 + Vt_N2 + Vt_N3 + Vt_N4;
+V_O = Vt_O1 + Vt_O2 + Vt_O3 + Vt_O4;
 V_T = Vt_T;
 
 % fprintf('### Tumor sanity check ###\n')
 % fprintf('Real volume: %.02f\n', pi * 4 / 3)
 % fprintf('Est. volume: %.02f\n', sum(in_tumor_flags) * dV)
 
-save('mlop_data2', 'D_N1', 'D_N2', 'D_N3', 'D_O1', 'D_O2', ...
-     'D_O3', 'D_T', 'Dt_N1', 'Dt_N2', 'Dt_N3', 'Dt_O1', ...
-     'Dt_O2', 'Dt_O3', 'Dt_T', 'C_T', 'C_N', 'C_O', 'V_T', 'V_N', 'V_O');
+save('mlop_data_time_wise', 'D_N1', 'D_N2', 'D_N3', 'D_N4', 'D_O1', 'D_O2', ...
+     'D_O3', 'D_O4', 'D_T', 'Dt_N1', 'Dt_N2', 'Dt_N3', 'Dt_N4', 'Dt_O1', ...
+     'Dt_O2', 'Dt_O3', 'Dt_O4', 'Dt_T', 'C_T', 'C_N', 'C_O', 'V_T', 'V_N', 'V_O');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function D = return_dmat(flag1, flag2, L)
